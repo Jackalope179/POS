e72b8dcfd38356dce9a3c2d98c73003032338289
Jackalope <nguyencanhhoangnh2019@gmail.com>
2021-11-03 13:49:13 +0700

    loop

---
+++ helpers/handlebars.js
@@ -0,0 +1,8 @@
+const Handlebars = require("handlebars");
+module.exports = (Handlebars) => {
+  Handlebars.registerHelper("times", function (n, block) {
+    var accum = "";
+    for (var i = 0; i < n; ++i) accum += block.fn(i);
+    return accum;
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+var MenuArray = [
+  "HAMBURGER",
+  "HAMBURGER",
+  "HAMBURGER",
+  "HAMBURGER",
+  "HAMBURGER",
+  "HAMBURGER",
+  "HAMBURGER",
+  "HAMBURGER",
+  "HAMBURGER",
+  "HAMBURGER",
+  "HAMBURGER",
+  "HAMBURGER",
+];
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Express",
+    MenuArray: MenuArray,
+    array: [1, 2, 3],
+  });
 });
-
-
 
 module.exports = router;
