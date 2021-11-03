36c083ac033fa6ffc2795ef4fa143e4513ba86f7
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2021-11-03 10:43:32 +0700

    something

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
+router.get('/error', function(req, res, next) {
+  res.render('error', { title: 'Express' });
+});
 
 module.exports = router;
