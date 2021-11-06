d885db43eeecff598e11ca37de4ae6cda894ea26
khangvt146 <khangvotan@gmail.com>
2021-11-06 22:06:30 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,22 @@
 var express = require("express");
 var router = express.Router();
 var foodapi = require("../public/api/food.json");
+var cartapi = require("../public/api/cart.json");
+
 
 /* GET home page. */
-router.get("/", async function (req, res, next) {
-  // let foodList = await food.getAllFood();
-  res.render("index", {
-    title: "POS restaurant",
-    MenuArray: foodapi,
-    array: [1, 2, 3],
-  });
+router.get("/", async function(req, res, next) {
+    // let foodList = await food.getAllFood();
+    res.render("index", {
+        title: "POS restaurant",
+        MenuArray: foodapi,
+        CartArray: cartapi,
+        array: [1, 2, 3],
+    });
 });
 
-router.get("/error", function (req, res, next) {
-  res.render("error", { title: "Express" });
+router.get("/error", function(req, res, next) {
+    res.render("error", { title: "Express" });
 });
 
 module.exports = router;
