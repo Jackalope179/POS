38416cb4077a4b30f4dfd8d6546ff5fff182fbf1
khangvt146 <khangvotan@gmail.com>
2021-11-06 09:43:34 +0700

    change

---
+++ public/js/menu/menu-list.js
@@ -0,0 +1,14 @@
+var addcarts = document.querySelectorAll('.label-icon-cart');
+
+for (var i = 0; i < addcarts.length; i++) {
+    addcarts[i].onclick = function(e) {
+        var tem = document.querySelector('.modal-container');
+        tem.classList.add('modal--active');
+    }
+}
+
+var outcarts = document.querySelector('.close__modal-icon');
+outcarts.onclick = function(e) {
+    var tem = document.querySelector('.modal-container');
+    tem.classList.remove('modal--active');
+}
