7aa7aa62050b84df7bae4269d085d15bd6e0de0a
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2021-11-06 22:57:56 +0700

    select category

--- public/js/menu/menu-list.js
+++ public/js/menu/menu-list.js
@@ -1,4 +1,123 @@
-// var foodapi = require("../../api/food.json");
+let food = [
+    {
+      "foodid": "6",
+      "name": "Nước lọc Dasani",
+      "price": "10000",
+      "foodDescribe": "nước uống",
+      "image": "./img/image-food4.jpg",
+      "category": "nước uống",
+      "isRecommended": "0"
+    },
+    {
+      "foodid": "7",
+      "name": "Bia Heneiken",
+      "price": "20000",
+      "foodDescribe": "nước uống",
+       "image": "./img/image-food5.jpg",
+      "category": "nước uống",
+      "isRecommended": "1"
+    },
+    {
+      "foodid": "8",
+      "name": "Bia Tiger",
+      "price": "20000",
+      "foodDescribe": "Fresh machiato",
+       "image": "./img/image-food6.jpg",
+      "category": "nước uống",
+      "isRecommended": "1"
+    },
+    {
+      "foodid": "9",
+      "name": "Coca cola",
+      "price": "15000",
+      "foodDescribe": "Medium BBQ beef",
+       "image": "./img/image-food7.jpg",
+      "category": "nước uống",
+      "isRecommended": "0"
+    },
+    {
+      "foodid": "10",
+      "name": "Pepsi",
+      "price": "15000",
+      "foodDescribe": "Masterpiece french rice",
+       "image": "./img/image-food8.jpg",
+      "category": "nước uống",
+      "isRecommended": "0"
+    },
+    {
+      "foodid": "11",
+      "name": "Bún bò Huế",
+      "price": "35000",
+      "foodDescribe": "Fresh orange juice",
+       "image": "./img/image-food9.jpg",
+      "category": "cơm bún phở",
+      "isRecommended": "0"
+    },
+    {
+      "foodid": "12",
+      "name": "Cơm tấm",
+      "price": "35000",
+      "foodDescribe": "Fresh pineapple juice ",
+       "image": "./img/image-food10.jpg",
+      "category": "cơm bún phở",
+      "isRecommended": "1"
+    },
+    {
+      "foodid": "13",
+      "name": "Phở gà",
+      "price": "35000",
+      "foodDescribe": "Fat cake, birthday cake",
+       "image": "./img/image-food11.jpg",
+      "category": "cơm bún phở",
+      "isRecommended": "1"
+    },
+    {
+      "foodid": "14",
+      "name": "Hamburger",
+      "price": "20000",
+      "foodDescribe": "Lurger Burger fat burger",
+       "image": "./img/image-food1.jpg",
+      "category": "thức ăn nhanh",
+      "isRecommended": "0"
+    },
+    {
+      "foodid": "15",
+      "name": "Pizza",
+      "price": "20000",
+      "foodDescribe": "Coffee cake mocha machiato",
+       "image": "./img/image-food12.jpg",
+      "category": "thức ăn nhanh",
+      "isRecommended": "1"
+    },
+    {
+      "foodid": "16",
+      "name": "Khoai tây chiên",
+      "price": "50000",
+      "foodDescribe": "Noodle for family, noodle",
+       "image": "./img/image-food13.jpg",
+      "category": "thức ăn nhanh",
+      "isRecommended": "1"
+    },
+    {
+      "foodid": "17",
+      "name": "Lẩu thái",
+      "price": "100000",
+      "foodDescribe": "Fat cake, birthday bread",
+       "image": "./img/image-food14.jpg",
+      "category": "lẩu",
+      "isRecommended": "1"
+    },
+    {
+      "foodid": "17",
+      "name": "Lẩu ếch",
+      "price": "100000",
+      "foodDescribe": "Fat cake, birthday bread",
+       "image": "./img/image-food15.jpg",
+      "category": "lẩu",
+      "isRecommended": "1"
+    }
+];
+  
 
 var addcarts = document.querySelectorAll('.label-icon-cart');
 
@@ -20,10 +139,35 @@ var outcarts = document.querySelector('.close__modal-icon');
 var categoryList = document.getElementsByClassName('img-text');
 for (var i = 0; i<categoryList.length;i++){
     categoryList[i].onclick = function(){
-        categoryListTemp = document.getElementsByClassName('img-text');
+        var categoryListTemp = document.getElementsByClassName('img-text');
         for (var i = 0;i<categoryListTemp.length;i++)
             categoryListTemp[i].className = 'img-text';
         this.classList.add('category-active');
+        let foodList = document.querySelector('.food_list-js');
+        let cateName = document.querySelector('.food_list_name-js');
+        cateName.innerText = this.innerText;
+        var temp = ``;
+        for (var i =0;i<food.length;i++){
+            if (food[i].category == this.innerText.toLowerCase()){
+                temp += `
+                    <div class="col-sm-6 col-md-4 col-lg-3">
+                        <div class="card card--best-seller">
+                            <img class="card-img-top" src=${food[i].image} alt="Card image cap" />
+                            <div class="card-body">
+                                <h5 class="card-title">${food[i].name}</h5>
+                                <div class="label-control">
+                                    <p class="card-text">GIÁ: ${food[i].price}</p>
+                                    <a href="#" class="label-icon-cart">
+                                        <i class="label-icon fas fa-cart-plus"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `
+            }
+        }
+        foodList.innerHTML = temp;
     };
 }
 
