d8745a38e85ed92dc4dbc49eff605d3900808023
PhongxTran <92942665+PhongxTran@users.noreply.github.com>
2021-11-12 20:00:17 +0700

    up login

--- routes/index.js
+++ routes/index.js
@@ -29,10 +29,33 @@ router.get("/datban", async function(req, res, next) {
     // let foodList = await food.getAllFood();
     res.render("tablebooking");
 });
+
+router.get("/login", async function(req, res, next) {
+    res.render("login/login");
+});
+
+router.get("/register", async function(req, res, next) {
+    res.render("register/register");
+});
+
+router.get("/register/confirmotp", async function(req, res, next) {
+    res.render("register/confirmotp");
+});
+
+router.get("/forgotpassword", async function(req, res, next) {
+    res.render("forgotpassword/forgotpassword");
+});
+
+router.get("/forgotpassword/confirmotp", async function(req, res, next) {
+    res.render("forgotpassword/confirmotp");
+});
+
 console.log(foodapi);
 
 router.get("/error", function(req, res, next) {
     res.render("error", { title: "Express" });
 });
+
+
 
 module.exports = router;
