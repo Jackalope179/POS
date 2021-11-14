a057775e08cc3735417025f3948aee09e732bf8f
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2021-11-14 19:22:55 +0700

    fix button mobile

--- public/js/payment.js
+++ public/js/payment.js
@@ -2,12 +2,12 @@
 var cartMobileButton = document.querySelector('.cart-mobile__button');
 var cartMobile = document.querySelector('.cart-mobile');
 cartMobileButton.onclick = function(){
-    cartMobile.style.display = 'flex';
-    this.style.display = "none";
+    cartMobile.classList.add('cart-mobile--active');
+    this.classList.remove('cart-mobile__button--active');
 };
 // close cart mobile 
 var cartMobileClose = cartMobile.querySelector('i');
 cartMobileClose.onclick = ()=>{
-    cartMobile.style.display = "none";
-    cartMobileButton.style.display = "block";
+    cartMobile.classList.remove('cart-mobile--active');
+    cartMobileButton.classList.add('cart-mobile__button--active');
 }
