d22129b3760fd7af265043fa9f2d6e9fef7404e3
Khoan <60871062+Khoan-IT@users.noreply.github.com>
2021-11-14 20:15:58 +0700

    add caculate money

--- public/js/payment.js
+++ public/js/payment.js
@@ -1,3 +1,21 @@
+$(document).ready(function(){
+    let total = document.querySelector('.total-amount').innerText;
+    document.querySelector('#credit-card > form > div:nth-child(5) > div.sub_money').innerHTML = total;
+    document.querySelector('#net-banking > div:nth-child(3) > div.sub_money').innerHTML = total;
+    document.querySelector('#direct-cash > div:nth-child(2) > div.sub_money').innerHTML = total;
+    price = parseInt(total.split(' ')[0]);
+    price_vat = price*0.1
+    document.querySelector('#credit-card > form > div:nth-child(6) > div.sub_money').innerHTML = String(price_vat)+'.000 VNĐ';
+    document.querySelector('#net-banking > div:nth-child(4) > div.sub_money').innerHTML = String(price_vat)+'.000 VNĐ';
+    document.querySelector('#direct-cash > div:nth-child(3) > div.sub_money').innerHTML = String(price_vat)+'.000 VNĐ';
+    document.querySelector('#credit-card > form > div.total_row > div.total_money').innerHTML = String(price+price_vat)+'.000 VNĐ';
+    document.querySelector('#net-banking > div.total_row > div.total_money').innerHTML = String(price+price_vat)+'.000 VNĐ';
+    document.querySelector('#direct-cash > div.total_row > div.total_money').innerHTML = String(price+price_vat)+'.000 VNĐ';
+
+
+});
+
+
 // open cart-mobile
 var cartMobileButton = document.querySelector('.cart-mobile__button');
 var cartMobile = document.querySelector('.cart-mobile');
@@ -11,3 +29,6 @@ cartMobileClose.onclick = ()=>{
     cartMobile.classList.remove('cart-mobile--active');
     cartMobileButton.classList.add('cart-mobile__button--active');
 }
+
+
+
