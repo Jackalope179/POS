b54ca3adffb719305632de69fc2e8e691ad974ef
Khoan <60871062+Khoan-IT@users.noreply.github.com>
2021-11-16 17:05:29 +0700

    Update payment

--- models/food.js
+++ models/food.js
@@ -3,7 +3,8 @@ let connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "pos",
+  database: "POS",
+  port: 3310
 });
 
 exports.getAllFood = async function () {
