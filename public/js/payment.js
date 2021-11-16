ff61b25380b9db58257efc0a00db593d0b3c4209
Khoan <60871062+Khoan-IT@users.noreply.github.com>
2021-11-16 16:53:35 +0700

    Update payment

--- public/js/payment.js
+++ public/js/payment.js
@@ -1,16 +1,24 @@
+function NumberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x)) x = x.replace(pattern, "$1.$2");
+    return x;
+  }
+  
+
 $(document).ready(function(){
     let total = document.querySelector('.total-amount').innerText;
     document.querySelector('#credit-card > form > div:nth-child(5) > div.sub_money').innerHTML = total;
     document.querySelector('#net-banking > div:nth-child(3) > div.sub_money').innerHTML = total;
     document.querySelector('#direct-cash > div:nth-child(2) > div.sub_money').innerHTML = total;
-    price = parseInt(total.split(' ')[0]);
+    price = parseInt(total.split(' ')[0]*1000);
     price_vat = price*0.1
-    document.querySelector('#credit-card > form > div:nth-child(6) > div.sub_money').innerHTML = String(price_vat)+'.000 VNĐ';
-    document.querySelector('#net-banking > div:nth-child(4) > div.sub_money').innerHTML = String(price_vat)+'.000 VNĐ';
-    document.querySelector('#direct-cash > div:nth-child(3) > div.sub_money').innerHTML = String(price_vat)+'.000 VNĐ';
-    document.querySelector('#credit-card > form > div.total_row > div.total_money').innerHTML = String(price+price_vat)+'.000 VNĐ';
-    document.querySelector('#net-banking > div.total_row > div.total_money').innerHTML = String(price+price_vat)+'.000 VNĐ';
-    document.querySelector('#direct-cash > div.total_row > div.total_money').innerHTML = String(price+price_vat)+'.000 VNĐ';
+    document.querySelector('#credit-card > form > div:nth-child(6) > div.sub_money').innerHTML = String(NumberWithCommas(price_vat))+' VNĐ';
+    document.querySelector('#net-banking > div:nth-child(4) > div.sub_money').innerHTML = String(NumberWithCommas(price_vat))+' VNĐ';
+    document.querySelector('#direct-cash > div:nth-child(3) > div.sub_money').innerHTML = String(NumberWithCommas(price_vat))+' VNĐ';
+    document.querySelector('#credit-card > form > div.total_row > div.total_money').innerHTML = String(NumberWithCommas(price_vat+price))+' VNĐ';
+    document.querySelector('#net-banking > div.total_row > div.total_money').innerHTML = String(NumberWithCommas(price_vat+price))+' VNĐ';
+    document.querySelector('#direct-cash > div.total_row > div.total_money').innerHTML = String(NumberWithCommas(price_vat+price))+' VNĐ';
 
 
 });
