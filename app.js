670e827c951c20ba75309b2536317ead746b50a9
Hoang Nguyen <nguyencanhhoangnh2019@gmail.com>
2021-11-18 23:03:22 +0700

    git add session

--- app.js
+++ app.js
@@ -7,10 +7,42 @@ var logger = require("morgan");
 var hbs = require("hbs");
 const exphbs = require("express-handlebars");
 
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+
+// const connection = require("./models/food")
+
+
+const options = { // setting connection options
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pos',
+};
+
+// let options = connection.connection;
+const sessionStore = new MySQLStore(options);
+
+
+app.use(
+    session({
+        secret: 'cookie_secret',
+        name: "POScookie",
+        login: 0,
+        cookie: {
+            maxAge: 60000
+        },
+        resave: false,
+        saveUninitialized: false,
+        store: sessionStore, // assigning sessionStore to the session
+    })
+);
+
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -26,19 +58,19 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -12,155 +12,135 @@ var foodModel = require("../models/food");
 const adminController = require("../controllers/adminController")
 
 // Menu home page
-router.get("/", async function (req, res, next) {
-  // console.log(req.body);
-  let foodList = await foodModel.getAllFood();
+router.get("/", async function(req, res, next) {
+    let foodList = await foodModel.getAllFood();
 
-  // food in first carousel
-  let numberofActive = [0, 1, 2];
-
-  // food in second carrousel
-  let numberofInactive = [3, 5, 6, 7];
-
-  let numberofActiveRender = numberofActive.map(function (item) {
-    return {
-      number: item,
-      categoryList: categoryApi[item],
+    let data = {
+        title: "POS restaurant",
+        foodListRender: foodList,
     };
-  });
+    console.log("CartArray", req.session.food);
+    // console.log("total", totalAmount)
+    if (req.session.login == 1) {
+        data = {
+            ...data,
+            login: 1,
+            CartArray: req.session.food,
+            totalAmount: req.session.totalAmount,
+        }
+    }
+    res.render("menu", data)
 
-  let numberofInactiveRender = numberofInactive.map(function (item) {
-    return {
-      number: item,
-      categoryList: categoryApi[item],
-    };
-  });
-
-  let foodListRender = foodList.map(function (item) {
-    return Object({
-      ...item,
-    });
-  });
-  res.render("menu", {
-    title: "POS restaurant",
-    foodListRender: foodListRender,
-    numberofActiveRender: numberofActiveRender,
-    numberofInactiveRender: numberofInactiveRender,
-  });
 });
+
+router.post("/payment", async function(req, res, next) {
+    req.session.login = 1;
+    res.render("payment", {
+        CartArray: req.session.food,
+        totalAmount: req.session.totalAmount,
+    });
+});
+router.get("/payment", async function(req, res, next) {
+    res.render("payment", {
+        CartArray: req.session.food,
+        totalAmount: req.session.totalAmount,
+    });
+});
+
 
 function NumberWithCommas(x) {
-  x = x.toString();
-  var pattern = /(-?\d+)(\d{3})/;
-  while (pattern.test(x)) x = x.replace(pattern, "$1.$2");
-  return x;
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x)) x = x.replace(pattern, "$1.$2");
+    return x;
 }
 
-router.post("/search", async function (req, res) {
-  let foodSearch = JSON.parse(JSON.stringify(req.body));
-  let searchResult = await foodModel.getFoodBySearch(foodSearch.search);
+router.post("/search", async function(req, res) {
+    let foodSearch = JSON.parse(JSON.stringify(req.body));
+    let searchResult = await foodModel.getFoodBySearch(foodSearch.search);
+    let foodList = await foodModel.getAllFood();
 
-  let foodList = await foodModel.getAllFood();
-  let numberofActive = [0, 1, 2];
-
-  // food in second carrousel
-  let numberofInactive = [3, 5, 6, 7];
-
-  let numberofActiveRender = numberofActive.map(function (item) {
-    return {
-      number: item,
-      categoryList: categoryApi[item],
+    let data = {
+        CartArray: req.session.food,
+        totalAmount: req.session.totalAmount,
+        title: "POS restaurant",
+        searchName: foodSearch.search,
+        search: true,
+        foodSearched: searchResult,
+        foodListRender: foodList,
     };
-  });
 
-  let numberofInactiveRender = numberofInactive.map(function (item) {
-    return {
-      number: item,
-      categoryList: categoryApi[item],
-    };
-  });
-
-  res.render("menu", {
-    title: "POS restaurant",
-    searchName: foodSearch.search,
-    search: true,
-    foodSearched: searchResult,
-    foodListRender: foodList,
-    numberofActiveRender: numberofActiveRender,
-    numberofInactiveRender: numberofInactiveRender,
-  });
+    if (req.session.login == 1) {
+        data = {
+            ...data,
+            login: 1,
+            CartArray: req.session.food,
+            totalAmount: req.session.totalAmount,
+        }
+    }
+    res.render("menu", data)
 });
 // Menu payment post
-router.post("/thanhtoan", async function (req, res, next) {
-  let foodapi = req.body;
-  let foodArray = foodapi.cartList.split("\n");
-  let foodjson = [];
-  foodArray.forEach(function (item) {
-    if (item != "") {
-      let foodAttribute = item.split(",");
-      foodjson.push({
-        name: foodAttribute[0],
-        image: foodAttribute[1],
-        quantity: foodAttribute[2],
-
-        price: Number(foodAttribute[3]?.split("\r")[0]).toLocaleString(
-          "vi-VN",
-          { style: "currency", currency: "VND" }
-        ),
-      });
-    }
-  });
-  res.render("payment", {
-    CartArray: foodjson,
-    totalAmount: NumberWithCommas(foodapi.totalPrice),
-  });
+router.post("/thanhtoan", async function(req, res, next) {
+    let foodapi = req.body;
+    let foodArray = foodapi.cartList.split("\n");
+    let foodjson = [];
+    foodArray.forEach(function(item) {
+        if (item != "") {
+            let foodAttribute = item.split(",");
+            foodjson.push({
+                name: foodAttribute[0],
+                image: foodAttribute[1],
+                quantity: foodAttribute[2],
+                price: NumberWithCommas(Number(foodAttribute[3].split("\r")[0])),
+            });
+        }
+    });
+    req.session.food = foodjson;
+    req.session.totalAmount = NumberWithCommas(foodapi.totalPrice)
+        // console.log(req.session.food);
+    res.render("payment", {
+        CartArray: req.session.food,
+        totalAmount: req.session.totalAmount,
+    });
 });
 
-router.get("/thanhtoan", async function (req, res, next) {
-  res.render("payment", {
-    title: "POS restaurant",
-  });
+router.get("/thanhtoan", async function(req, res, next) {
+    res.render("payment", {
+        CartArray: req.session.food,
+        totalAmount: req.session.totalAmount,
+    });
 });
 
-router.get("/datban", async function (req, res, next) {
-  res.render("tablebooking");
+router.get("/datban", async function(req, res, next) {
+    res.render("tablebooking");
 });
 
-router.get("/login", async function (req, res, next) {
-  res.render("login/login");
-});
-
-router.post("/", async function (req, res, next) {
-  res.render("menu", {
-    title: "POS restaurant",
-    MenuArray: foodapi,
-    CartArray: cartapi,
-    array: [1, 2, 3],
-  });
-});
-
-router.get("/register", async function (req, res, next) {
-  res.render("register/register");
-});
-
-router.post("/register/confirmotp", async function (req, res, next) {
-  res.render("register/confirmotp");
-});
-
-router.post("/login", async function (req, res, next) {
-  res.render("login/login");
-});
-
-router.get("/forgotpassword", async function (req, res, next) {
-  res.render("forgotpassword/forgotpassword");
-});
-
-router.post("/forgotpassword/confirmotp", async function (req, res, next) {
-  res.render("forgotpassword/confirmotp");
+router.get("/login", async function(req, res, next) {
+    res.render("login/login");
 });
 
 
 
+router.get("/register", async function(req, res, next) {
+    res.render("register/register");
+});
+
+router.post("/register/confirmotp", async function(req, res, next) {
+    res.render("register/confirmotp");
+});
+
+router.post("/login", async function(req, res, next) {
+    res.render("login/login");
+});
+
+router.get("/forgotpassword", async function(req, res, next) {
+    res.render("forgotpassword/forgotpassword");
+});
+
+router.post("/forgotpassword/confirmotp", async function(req, res, next) {
+    res.render("forgotpassword/confirmotp");
+});
 
 /////// ADMIN //////////////
 
@@ -183,8 +163,8 @@ router.post("/menu-admin/deleteFood", adminController.deleteFood);
 //     res.render("forgotpassword/confirmotp");
 // });
 
-router.get("/error", function (req, res, next) {
-  res.render("error", { title: "Express" });
+router.get("/error", function(req, res, next) {
+    res.render("error", { title: "Express" });
 });
 
 module.exports = router;
