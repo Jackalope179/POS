7167451b1e520de36269b7312c16d379b490e187
khangvt146 <khangvotan@gmail.com>
2021-11-18 15:42:07 +0700

    Updated pre-loader

--- public/js/preloader.js
+++ public/js/preloader.js
@@ -3,8 +3,8 @@
 	======================================*/
 
 //After 2s preloader is fadeOut
-$('.preloader').delay(800).fadeOut('slow');
+$('.preloader').delay(700).fadeOut('slow');
 setTimeout(function() {
     //After 2s, the no-scroll class of the body will be removed
     $('body').removeClass('no-scroll');
-}, 800); //Here you can change preloader time
+}, 700); //Here you can change preloader time
