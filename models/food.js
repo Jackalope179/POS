e3a7ce8ea1c64ff0d78a71f313e87a5f916bff10
duckhoan.hcmut <duckhoan.work@gmail.com>
2021-11-19 14:32:53 +0700

    Update login

--- models/food.js
+++ models/food.js
@@ -3,8 +3,8 @@ let connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "pos",
-  // port: 3310
+  database: "POS",
+  port: 3310
 });
 
 exports.getAllFood = async function () {
@@ -34,3 +34,27 @@ exports.updateOneFood = async function (values) {
     'UPDATE food SET ? WHERE foodId = ?', [values, values.foodId]
   );
 };
+
+exports.CheckAccount = async function(Numphone, password) {
+  return await connection.awaitQuery(
+      `SELECT * FROM account WHERE phone = '${Numphone}' AND password = '${password}'`
+  )
+}
+
+exports.CheckPhone = async function(Numphone) {
+  return await connection.awaitQuery(
+      `SELECT * FROM account WHERE phone = '${Numphone}'`
+  )
+}
+
+exports.InsertCustomer = async function(Phone, Pass, Name) {
+  return await connection.awaitQuery(
+      `INSERT INTO account (password, fullname, phone) VALUES ('${Pass}', '${Name}', '${Phone}')`
+  )
+}
+
+exports.UpdateCustomer = async function(Phone, Pass) {
+  return await connection.awaitQuery(
+      `UPDATE account SET password ='${Pass}' WHERE phone = '${Phone}'`
+  )
+}
--- routes/index.js
+++ routes/index.js
@@ -139,25 +139,139 @@ router.post("/", async function (req, res, next) {
   });
 });
 
-router.get("/register", async function (req, res, next) {
+router.get("/register", async function(req, res, next) {
   res.render("register/register");
 });
 
-router.post("/register/confirmotp", async function (req, res, next) {
-  res.render("register/confirmotp");
+router.post("/register", async function(req, res, next) {
+  res.render("register/register");
 });
 
-router.post("/login", async function (req, res, next) {
+router.post("/register/checkPhone", async(req, res) => {
+  let phoneNum = req.body.phone;
+  await foodModel.CheckPhone(String(phoneNum)).then(function(result) {
+      // console.log(result);
+      if (result.length > 0) {
+          res.json({
+              status: "FOUND",
+              data: result[0],
+          });
+      } else {
+          res.json({
+              status: "NOT_FOUND",
+          });
+      }
+  });
+});
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var OTP = 0;
+
+router.post("/register/confirmotp", async function(req, res, next) {
+  let phoneNum = req.body.phone;
+  let pass = req.body.password;
+  let name = req.body.fullname;
+  OTP = getRndInteger(100000, 999999);
+  console.log(OTP);
+  res.render("register/confirmotp", { phone: phoneNum, password: pass, name: String(name) });
+});
+
+
+
+router.post("/register/checkOTP", async(req, res) => {
+  let phoneNum = req.body.phone;
+  let password = req.body.password;
+  let name = req.body.name;
+  let otp = req.body.otp;
+  console.log(OTP);
+  if (parseInt(otp) === OTP) {
+      await foodModel.InsertCustomer(String(phoneNum), String(password), String(name));
+      res.json({
+          status: "TRUE",
+      });
+  } else {
+      res.json({
+          status: "FAIL",
+      });
+  }
+});
+
+
+router.post("/login", async function(req, res, next) {
   res.render("login/login");
 });
 
-router.get("/forgotpassword", async function (req, res, next) {
+router.post("/login/checkPhone", async(req, res) => {
+  let phoneNum = req.body.phone;
+  let pass = req.body.password;
+  await foodModel.CheckAccount(String(phoneNum), String(pass)).then(function(result) {
+      if (result.length > 0) {
+          res.json({
+              status: "FOUND",
+              data: result[0],
+          });
+      } else {
+          res.json({
+              status: "NOT_FOUND",
+          });
+      }
+  });
+});
+
+router.get("/forgotpassword", async function(req, res, next) {
   res.render("forgotpassword/forgotpassword");
 });
 
-router.post("/forgotpassword/confirmotp", async function (req, res, next) {
-  res.render("forgotpassword/confirmotp");
+router.post("/forgotpassword", async function(req, res, next) {
+  res.render("forgotpassword/forgotpassword");
 });
+
+router.post("/forgotpassword/checkPhone", async(req, res) => {
+  let phoneNum = req.body.phone;
+  await foodModel.CheckPhone(String(phoneNum)).then(function(result) {
+      console.log(result);
+      if (result.length > 0) {
+          res.json({
+              status: "FOUND",
+              data: result[0],
+          });
+      } else {
+          res.json({
+              status: "NOT_FOUND",
+          });
+      }
+  });
+});
+
+
+router.post("/forgotpassword/confirmotp", async function(req, res, next) {
+  let phoneNum = req.body.phone;
+  let new_pass = req.body.password;
+  OTP = getRndInteger(100000, 999999);
+  console.log(OTP);
+  res.render("forgotpassword/confirmotp", { phone: phoneNum, password: new_pass });
+});
+
+router.post("/forgotpassword/checkOTP", async(req, res) => {
+  let phoneNum = req.body.phone;
+  let password = req.body.new_pass;
+  let otp = req.body.otp;
+  if (parseInt(otp) === OTP) {
+      await foodModel.UpdateCustomer(String(phoneNum), String(password));
+      res.json({
+          status: "TRUE",
+      });
+  } else {
+      res.json({
+          status: "FAIL",
+      });
+  }
+});
+
+
 
 
 
