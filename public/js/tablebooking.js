0a75d998054029354bd103b9e6f5a4c9a2f1ff56
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2021-11-19 21:49:05 +0700

    modal table

--- public/js/tablebooking.js
+++ public/js/tablebooking.js
@@ -1,2 +1,18 @@
 
 document.querySelectorAll('.header .nav-link')[1].classList.add('active');
+
+
+function SetClose(){
+    var closeBtn = document.querySelectorAll('.table-close')
+    closeBtn.forEach(btn=>{
+        btn.onclick = ()=>{
+            document.querySelector('.modal').classList.add('modal-table'); 
+        }
+    })
+}
+
+document.querySelector('.modal .btn-secondary').onclick = ()=>{
+    document.querySelector('.modal').classList.remove('modal-table'); 
+}
+
+SetClose();
