297ea67c35a701e7b5718202a01087eca0c6b137
khangvt146 <khangvotan@gmail.com>
2021-11-21 09:31:06 +0700

    updated

--- models/booking.js
+++ models/booking.js
@@ -3,13 +3,8 @@ let connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-<<<<<<< HEAD
     database: "pos",
     port: 3306
-=======
-    database: "pos"
-        // port: 3310
->>>>>>> d3d78b024f98c05f8996a8851437ff3e754504e2
 });
 
 exports.getBooking = async function(phone) {
