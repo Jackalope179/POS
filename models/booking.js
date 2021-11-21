7f5afe4c110941a6e0c67191613137a4b7ba108f
Hoang Nguyen <nguyencanhhoangnh2019@gmail.com>
2021-11-21 17:54:41 +0700

    fix menu controller

--- models/booking.js
+++ models/booking.js
@@ -4,7 +4,7 @@ let connection = mysql.createConnection({
     user: "root",
     password: "",
     database: "pos",
-    port: 3310
+    // port: 3310
 });
 
 exports.getBooking = async function(phone) {
