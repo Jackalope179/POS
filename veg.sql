7ccb6ea07ff3cebb36d504795653d493c3ea2982
hiepnguyen3872 <59463682+hiepnguyen3872@users.noreply.github.com>
2021-11-27 09:14:41 +0700

    add veg.sql

---
+++ veg.sql
@@ -0,0 +1,7 @@
+INSERT INTO `food` (`foodId`, `category`, `foodDescribe`, `price`, `name`, `image`, `isRecommended`) VALUES
+(13, 'thức ăn chay', 'Nước lẩu được tạo ra từ vị ngọ', 50000, 'Lẩu nấm chay', './img/veg-1.jpg', 1),
+(14, 'thức ăn chay', 'Mì xào chay không chỉ thơm ngo', 30000, 'Mì xào chay', './img/veg-2.jpg', 1),
+(15, 'thức ăn chay', 'Bún bò Huế thơm ngon, đậm vị v', 25000, 'Bún bò chay', './img/veg-3.jpg', 0),
+(16, 'thức ăn chay', 'Nấm giúp cho nước dùng ngon ng', 20000, 'Canh nấm chay', './img/veg-4.jpg', 1),
+(17, 'thức ăn chay', 'Món bún Thái chay là sự kết hợ', 20000, 'Bún thái chay', './img/veg-5.jpg', 0);
+
